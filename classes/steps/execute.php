61e8ab2e48c93a15b0917d7da267fda5a2ea1736
Tharkun <jerem.poulain@gmail.com>
2012-12-11 11:35:44 +0100

    Revert

--- classes/steps/execute.php
+++ classes/steps/execute.php
@@ -105,7 +105,7 @@ class execute extends step implements definitions\php\observable
 
             $stdOut = $task->getStdOut();
 
-            if (1||trim($stdOut) !== '')
+            if (trim($stdOut) !== '')
             {
                 foreach (explode("\n", $stdOut) as $s)
                 {
