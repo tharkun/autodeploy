b28d91422b063b5240d26c09f1bb3a082f1105f5
Tharkun <jerem.poulain@gmail.com>
2013-02-01 00:11:56 +0100

    eZ Publish autoload task should be aggregated

--- classes/profiles/ezpublish/commands/ezpgenerateautoloads.php
+++ classes/profiles/ezpublish/commands/ezpgenerateautoloads.php
@@ -4,7 +4,7 @@ namespace autodeploy\profiles\ezpublish\commands;
 
 use autodeploy\commands\php;
 
-class ezpgenerateautoloads extends php
+class ezpgenerateautoloads extends php implements aggregatable
 {
 
     protected $options = array();
@@ -15,6 +15,29 @@ class ezpgenerateautoloads extends php
     public function __toString()
     {
         return parent::__toString() . ' ' . $this->cleanPath('bin/php/ezpgenerateautoloads.php') . ' -e -p';
+    }
+
+    /**
+     * @param aggregatable $object
+     * @return ezpgenerateautoloads
+     */
+    public function aggregate(aggregatable $object)
+    {
+        return $this;
+    }
+
+    /**
+     * @param aggregatable $object
+     * @return bool
+     */
+    public function isAggregatableWith(aggregatable $object)
+    {
+        if (get_class($object) === get_class($this))
+        {
+            return true;
+        }
+
+        return false;
     }
 
 }
